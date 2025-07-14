9bf9c4d781618b3dee1398d2994ff94e018e4add
farisubuntu <farisubuntu@gmail.com>
2025-07-14 12:32:10 +0300

    update...

---
+++ src/components/english-sounds.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+export function EnglishSoundsCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>English Sounds</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <h2>English Sounds</h2>
+        <p>TODO: Add English Sounds Flash Cards</p>
+      </CardContent>
+    </Card>
+  );
+}
--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -8,15 +8,18 @@ import { ChapterSheet } from "./chapter-sheet";
 import { LessonSheet } from "./lesson-sheet";
 import { UnitCards } from "./unit-cards";
 import { AlphabetsCard } from "./alphabets-card";
+import { EnglishSoundsCard } from "./english-sounds";
 
 export function SectionCards() {
   const [selectedSection, setSelectedSection] =
     React.useState("sound-and-script");
   const style = {
+    width: "100%",
+    height: "100%",
     border: "1px solid #ccc",
     borderRadius: "10px",
     backgroundColor: "#fff",
-    padding: "10px",
+    padding: "5px",
     boxShadow: "0 0 10px rgba(0,0,0,0.1)",
   };
   const selectedStyle = {
@@ -28,49 +31,58 @@ export function SectionCards() {
     setSelectedSection(value);
   };
   return (
-      <Tabs
-        defaultValue={selectedSection}
-        className="w-screen h-screen overflow-y-auto"
-        style={style}
-        onValueChange={handleTabChange}
-      >
-        <TabsList className="w-full mx-2xl">
-          <TabsTrigger
-            value="sound-and-script"
-            className="hover:bg-gray-100 hover:cursor-pointer"
-            style={selectedSection === "sound-and-script" ? selectedStyle : {}}
-          >
-            Sound and Script
-          </TabsTrigger>
-          <TabsTrigger
-            value="units"
-            className="hover:bg-gray-100 hover:cursor-pointer"
-            style={selectedSection === "units" ? selectedStyle : {}}
-          >
-            Units
-          </TabsTrigger>
-          <TabsTrigger
-            value="alphabets"
-            className="hover:bg-gray-100 hover:cursor-pointer"
-            style={selectedSection === "alphabets" ? selectedStyle : {}}
-          >
-            Alphabets
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="sound-and-script">
-          {/* <JsonViewerComponent data={SoundAndScriptData} /> */}
-          <div className="flex flex-cols-1 gap-4">
-            {SoundAndScriptData.map((lesson) => (
-              <LessonSheet key={lesson.lesson} lesson={lesson} />
-            ))}
-          </div>
-        </TabsContent>
-        <TabsContent value="units">
-          <UnitCards units={UnitsData} />
-        </TabsContent>
-        <TabsContent value="alphabets">
-          <AlphabetsCard />
-        </TabsContent>
-      </Tabs>
+    <Tabs
+      defaultValue={selectedSection}
+      onValueChange={handleTabChange}
+      className="min-w-screen min-h-screen"
+    >
+      <TabsList className="">
+        <TabsTrigger
+          value="sound-and-script"
+          className="hover:bg-gray-100 hover:cursor-pointer"
+          style={selectedSection === "sound-and-script" ? selectedStyle : {}}
+        >
+          Sound and Script
+        </TabsTrigger>
+        <TabsTrigger
+          value="units"
+          className="hover:bg-gray-100 hover:cursor-pointer"
+          style={selectedSection === "units" ? selectedStyle : {}}
+        >
+          Units
+        </TabsTrigger>
+        <TabsTrigger
+          value="alphabets"
+          className="hover:bg-gray-100 hover:cursor-pointer"
+          style={selectedSection === "alphabets" ? selectedStyle : {}}
+        >
+          Alphabets
+        </TabsTrigger>
+        <TabsTrigger
+          value="english-sounds"
+          className="hover:bg-gray-100 hover:cursor-pointer"
+          style={selectedSection === "english-sounds" ? selectedStyle : {}}
+        >
+          English Sounds
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="sound-and-script" style={style}>
+        {/* <JsonViewerComponent data={SoundAndScriptData} /> */}
+        <div className="flex flex-cols-1 gap-4">
+          {SoundAndScriptData.map((lesson) => (
+            <LessonSheet key={lesson.lesson} lesson={lesson} />
+          ))}
+        </div>
+      </TabsContent>
+      <TabsContent value="units">
+        <UnitCards units={UnitsData} />
+      </TabsContent>
+      <TabsContent value="alphabets">
+        <AlphabetsCard />
+      </TabsContent>
+      <TabsContent value="english-sounds">
+        <EnglishSoundsCard />
+      </TabsContent>
+    </Tabs>
   );
 }
