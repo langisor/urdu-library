f80922d9e8899e2e175d2c3e3a9e8d2e0767c97f
langisor <langisor-app@proton.me>
2025-07-23 18:40:30 +0300

    update...

--- src/app/mondly/page.tsx
+++ src/app/mondly/page.tsx
@@ -1,7 +1,14 @@
+"use client"
+import { vocabs, getVocabularyItems, getTotalWords } from "@/data/mondly-urdu-vocas"
+import { JsonViewerComponent } from "@/components/json-viewer"
+function showVocab(vocab: any){
+    console.log(vocab)
+}
 export default function MondlyPage(){
     return (
         <div>
             <h1>Mondly</h1>
+            <JsonViewerComponent data={ getTotalWords()} />
         </div>
     )
 }
