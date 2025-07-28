d00685f072f63e8198d1c30945298ead41c7c04e
langisor <langisor-app@proton.me>
2025-07-28 22:03:25 +0300

    update...

--- src/app/api/json/course-book/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import BookData from "@/data/course-book/toc-and-media.json";
-import { IBookData } from "@/data/course-book/ts-definition";
-export async function GET(request: NextRequest) {
-  const bookData = BookData as unknown as IBookData;
-
-  return NextResponse.json(bookData);
-}
--- src/app/api/media/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(request: NextRequest) {
-  const res = {
-    message: "Hello World",
-    path: "/api/media",
-  };
-  return NextResponse.json(res);
-}
