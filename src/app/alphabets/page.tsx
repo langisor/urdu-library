68559504bd95d9cdb5428daa8deb5232138ea03e
langisor <langisor-app@proton.me>
2025-07-28 23:53:18 +0300

    update...

--- src/app/alphabets/page.tsx
+++ src/app/alphabets/page.tsx
@@ -1,3 +1,11 @@
+import { AlphabetsCard } from "@/components/alphabets/alphabets-card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 export default function AlphabetsPage() {
-  return <h1 className="text-3xl font-bold">Alphabets Page</h1>;
+  return (
+    <ScrollArea className="h-screen">
+      <div className="flex flex-col gap-3">
+        <AlphabetsCard />
+      </div>
+    </ScrollArea>
+  );
 }
