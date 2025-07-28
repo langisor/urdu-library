279e517e1572cb8f66ee764a34a35c7224088259
langisor <langisor-app@proton.me>
2025-07-28 15:39:29 +0300

    update...

--- src/app/course-book/page.tsx
+++ src/app/course-book/page.tsx
@@ -5,14 +5,17 @@ import { Loading } from "@/components/loading";
 import BookNavigator from "@/components/course-book-nav/navigator";
 import { MediaDirectoryTree } from "@/data/helpers/utils";
 export default function CourseBookPage() {
-  const courseBook = useCourseBook();
+  const { data: courseBook, error, isLoading } = useCourseBook();
+  if (isLoading) return <Loading />;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!courseBook) return <div>No data available</div>;
   
   console.log("courseBook", courseBook);
   return (
     <Suspense fallback={<Loading />}>
       <div>
         <h1>Course Book</h1>
-        <BookNavigator initialData={courseBook.data as MediaDirectoryTree} />
+        <BookNavigator initialData={courseBook as MediaDirectoryTree} />
       </div>
     </Suspense>
   );
