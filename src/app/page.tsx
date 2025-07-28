c11e8a6431d8670e31385135605b9098e9b11478
langisor <langisor-app@proton.me>
2025-07-28 23:28:02 +0300

    update...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Home Page</h1>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/course-book">Course Book</Link>
+      <Link href="/alphabets">Alphabets</Link>
+      <Link href="/urdu-script-book">Urdu Script Book</Link>
+    </div>
+  );
 }
