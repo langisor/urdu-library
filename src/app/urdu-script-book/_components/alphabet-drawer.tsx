aa14125bc7464da49f0af63642d15f516c658922
farisubuntu <farisubuntu@gmail.com>
2025-07-31 01:11:20 +0300

    update...

--- src/app/urdu-script-book/_components/alphabet-drawer.tsx
+++ src/app/urdu-script-book/_components/alphabet-drawer.tsx
@@ -6,6 +6,7 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
+  DrawerClose,
 } from "@/components/ui/drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
@@ -35,7 +36,7 @@ export default function AlphabetDrawerTable() {
         {alphabetsData.alphabets.map((alphabet) => (
           <DrawerTrigger
             key={alphabet.letter_alone}
-            className="w-full text-center flex items-center justify-center h-48 bg-gray-100 hover:bg-gray-200 transition-colors"
+            className="h-48 bg-gray-100 hover:bg-gray-200 transition-colors"
             onClick={() => {
               setCurrentAlphabetIndex(
                 alphabetsData.alphabets.indexOf(alphabet)
@@ -60,41 +61,47 @@ export default function AlphabetDrawerTable() {
   const renderDrawerContent = () => {
     const alphabet = alphabetsData.alphabets[currentAlphabetIndex];
     return (
-      <DrawerContent className="sm:h-screen w-full overflow-y-auto">
-        <DrawerHeader>
+      <DrawerContent className="min-h-screen w-full overflow-y-auto">
+        <DrawerHeader className="flex flex-row gap-3">
           <DrawerTitle className="phonetic">
             {alphabet.name_romanized}
           </DrawerTitle>
+          <Button
+            onClick={() => {
+              const audio = new Audio(alphabet.audio_link);
+              audio.play();
+            }}
+            className="w-24 h-16 bg-blue-500 text-white hover:bg-blue-600"
+          >
+            Play Sound
+          </Button>
         </DrawerHeader>
         <div className="p-4 my-4">
           <div className="flex flex-col space-y-4">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
               <Card>
                 <CardContent className="text-center">
-                  <h3 className="big-en">
-                    Initial Form
-                  </h3>
-                  <h3 className="big-ur">
-                    {alphabet.initial_form}
-                  </h3>
+                  <h3 className="big-en">Initial Form</h3>
+                  <h3 className="big-ur">{alphabet.initial_form}</h3>
                 </CardContent>
               </Card>
               <Card>
                 <CardContent className="text-center">
-                  <h3 className="big-en">
-                    Medial Form
-                  </h3>
-                  <h3 className="big-ur">
-                    {alphabet.medial_form}
-                  </h3>
+                  <h3 className="big-en">Medial Form</h3>
+                  <h3 className="big-ur">{alphabet.medial_form}</h3>
                 </CardContent>
               </Card>
               <Card>
                 <CardContent className="text-center">
-                  <h3 className="big-en">
-                    Final Form
-                  </h3>
+                  <h3 className="big-en">Final Form</h3>
                   <p className="big-ur">{alphabet.final_form}</p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardContent className="text-center mx-auto my-auto">
+                  <DrawerClose asChild className="flex flex-col">
+                    <Button onClick={() => setIsOpen(false)} className="w-48 h-16 text-3xl">Close</Button>
+                  </DrawerClose>
                 </CardContent>
               </Card>
             </div>
@@ -107,23 +114,12 @@ export default function AlphabetDrawerTable() {
               </CardContent>
             </Card>
           </div>
-          <div className="mt-4">
-            <Button
-              onClick={() => {
-                const audio = new Audio(alphabet.audio_link);
-                audio.play();
-              }}
-              className="mt-4 w-full bg-blue-500 text-white hover:bg-blue-600"
-            >
-              Play Sound
-            </Button>
-          </div>
         </div>
       </DrawerContent>
     );
   };
   return (
-    <Drawer open={isOpen} onOpenChange={setIsOpen}>
+    <Drawer open={isOpen} onOpenChange={setIsOpen} direction="top">
       <div className="p-4">{renderTriggers()}</div>
       {renderDrawerContent()}
     </Drawer>
--- src/app/urdu-script-book/_components/alphabets-table.tsx
+++ src/app/urdu-script-book/_components/alphabets-table.tsx
@@ -32,16 +32,25 @@ export function AlphabetsTable() {
   };
   return (
     <div className="">
-      <h1>{alphabetsData.title}</h1>
       <Table className="w-full">
         <TableHeader>
           <TableRow className="bg-zinc-700 text-xl font-bold ">
-            <TableHead className="text-white">Letter Alone</TableHead>
-            <TableHead className="text-white">Initial Form</TableHead>
-            <TableHead className="text-white">Medial Form</TableHead>
-            <TableHead className="text-white">Final Form</TableHead>
-            <TableHead className="text-white">Name (Romanized)</TableHead>
-            <TableHead className="text-white">
+            <TableHead className="border w-full text-center bg-slate-600 text-white text-center self-center ">
+              Letter Alone
+            </TableHead>
+            <TableHead className="border w-full text-center bg-slate-600 text-white text-center self-center ">
+              Initial Form
+            </TableHead>
+            <TableHead className="border w-full text-center bg-slate-600 text-white text-center self-center ">
+              Medial Form
+            </TableHead>
+            <TableHead className="border w-full text-center bg-slate-600 text-white text-center self-center ">
+              Final Form
+            </TableHead>
+            <TableHead className="border w-full text-center bg-slate-600 text-white text-center self-center ">
+              Name (Romanized)
+            </TableHead>
+            <TableHead className="border w-full text-center bg-slate-600 text-white text-center self-center ">
               Basic Sound (Romanized)
             </TableHead>
             <TableHead className="hidden">Audio</TableHead>
@@ -51,14 +60,25 @@ export function AlphabetsTable() {
           {alphabetsData.alphabets.map((alphabet, index) => (
             <TableRow
               key={index}
-              className="hover:bg-gray-100"
+              className="hover:bg-gray-100 p-2"
               onClick={() => handlePlay(alphabet.audio_link)}
             >
-              <TableCell className="ur">{alphabet.letter_alone}</TableCell>
-              <TableCell className="ur">{alphabet.initial_form}</TableCell>
-              <TableCell className="ur">{alphabet.medial_form}</TableCell>
-              <TableCell className="ur">{alphabet.final_form}</TableCell>
-              <TableCell className="ur">{alphabet.name_romanized}</TableCell>
+              <TableCell className="flex gap-2">
+                <span className="text-xl border rounded-full text-white bg-green-600 w-8 text-center">
+                  {index + 1}
+                </span>
+                <span className="ur text-lg">{alphabet.letter_alone}</span>
+              </TableCell>
+              <TableCell className="ur text-4xl text-center">
+                <span className="">{alphabet.initial_form}</span>
+              </TableCell>
+              <TableCell className="ur text-4xl text-center">
+                <span className="">{alphabet.medial_form}</span>
+              </TableCell>
+              <TableCell className="ur text-4xl text-center">
+                <span className="">{alphabet.final_form}</span>
+              </TableCell>
+              <TableCell className="">{alphabet.name_romanized}</TableCell>
               <TableCell className="">
                 {alphabet.basic_sound_romanized}
               </TableCell>
@@ -72,7 +92,7 @@ export function AlphabetsTable() {
                 >
                   {isPlaying ? "Playing..." : "Play Sound"}
                 </Button>
-                <audio ref={audioRef} />
+                <audio ref={audioRef} playsInline autoPlay={false} />
               </TableCell>
             </TableRow>
           ))}
