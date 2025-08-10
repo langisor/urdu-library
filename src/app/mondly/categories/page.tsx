55d9c936c203774fcb19452201ae577dc3bab312
farisubuntu <farisubuntu@gmail.com>
2025-08-10 20:28:38 +0300

    update...

--- src/app/mondly/categories/page.tsx
+++ src/app/mondly/categories/page.tsx
@@ -34,7 +34,6 @@ export default async function CategoriesPage() {
       <hr />
       
       <CategoriesCards categories={categories} />
-      {/* <JsonViewer value={categories} /> */}
     </div>
   );
 }
