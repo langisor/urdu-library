05445bd621f6bd54faffbc1e0cf30b94e53deb67
farisubuntu <farisubuntu@gmail.com>
2025-08-14 02:47:09 +0300

    update...

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,14 +1,21 @@
-import * as React from "react"
+import * as React from "react";
 
-// --- Custom useMediaQuery hook from your provided file ---
-export const useMediaQuery = (query: string): boolean => {
-  const [matches, setMatches] = React.useState(false);
+const MOBILE_BREAKPOINT = 768;
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
+    undefined
+  );
+
   React.useEffect(() => {
-    const mediaQuery = window.matchMedia(query);
-    setMatches(mediaQuery.matches);
-    const handler = (event: MediaQueryListEvent) => setMatches(event.matches);
-    mediaQuery.addEventListener("change", handler);
-    return () => mediaQuery.removeEventListener("change", handler);
-  }, [query]);
-  return matches;
-};
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+    mql.addEventListener("change", onChange);
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    return () => mql.removeEventListener("change", onChange);
+  }, []);
+
+  return !!isMobile;
+}
