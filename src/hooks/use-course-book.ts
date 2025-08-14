3d25933b9d4944ac7717f1b73fbcb775edc1b820
farisubuntu <farisubuntu@gmail.com>
2025-08-14 03:06:23 +0300

    update...

--- src/hooks/use-course-book.ts
+++
@@ -1,8 +0,0 @@
-import SWR from "swr";
-import { IBookData } from "@/data/course-book/ts-definition";
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
-export const useCourseBook = () => {
-  const { data, error, isLoading } = SWR<IBookData>("/api/json/course-book", fetcher);
-  return { data, error, isLoading };
-};
