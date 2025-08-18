21b1af521a51448c7c6f43bc1536d8f332b7caca
farisubuntu <farisubuntu@gmail.com>
2025-08-18 18:34:43 +0300

    update...

--- src/app/mondly/categories/page.tsx
+++ src/app/mondly/categories/page.tsx
@@ -1,17 +1,9 @@
-import { promises } from "fs";
+ 
 import { CategoriesCards } from "./_components/categories-cards";
-import path from "path";
+ 
 import { CategoryItem } from "../_types/data-services";
-const file = "src/app/mondly/_data/all.json";
 import { queryClient } from "@/lib/postgres-client";
-// async function getCategories() {
-//   const filepath = path.join(process.cwd(), file);
-//   const data = await promises.readFile(filepath, "utf-8");
-//   const parsedData = JSON.parse(data) as { data: { category: CategoryItem }[] };
-//   const categories: CategoryItem[] = [];
-//   for (const item of parsedData.data) categories.push({ ...item.category });
-//   return categories;
-// }
+ 
 async function getCategories() {
   const categories = await queryClient`
   SELECT * FROM "Category"
