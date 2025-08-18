ccf595abbd13343c35e8e700b065552bd2fcae99
farisubuntu <farisubuntu@gmail.com>
2025-08-18 12:33:34 +0300

    update...

--- src/app/mondly/categories/page.tsx
+++ src/app/mondly/categories/page.tsx
@@ -4,14 +4,20 @@ import path from "path";
 import { DynamicBreadcrumb } from "../_components/dynamic-breadcrumb";
 import { CategoryItem } from "../_types/data-services";
 const file = "src/app/mondly/_data/all.json";
-
+import { queryClient } from "@/lib/postgres-client";
+// async function getCategories() {
+//   const filepath = path.join(process.cwd(), file);
+//   const data = await promises.readFile(filepath, "utf-8");
+//   const parsedData = JSON.parse(data) as { data: { category: CategoryItem }[] };
+//   const categories: CategoryItem[] = [];
+//   for (const item of parsedData.data) categories.push({ ...item.category });
+//   return categories;
+// }
 async function getCategories() {
-  const filepath = path.join(process.cwd(), file);
-  const data = await promises.readFile(filepath, "utf-8");
-  const parsedData = JSON.parse(data) as { data: { category: CategoryItem }[] };
-  const categories: CategoryItem[] = [];
-  for (const item of parsedData.data) categories.push({ ...item.category });
-  return categories;
+  const categories = await queryClient`
+  SELECT * FROM "Category"
+  `;
+  return categories[0] as CategoryItem[];
 }
 
 export default async function CategoriesPage() {
