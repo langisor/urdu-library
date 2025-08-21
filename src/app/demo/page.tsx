e2df28c8b9456f5fb26274fd6675c288283b1a6e
farisubuntu <farisubuntu@gmail.com>
2025-08-21 11:39:31 +0300

    update...

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useTune } from "./_components/use-tune";
+import { Button } from "@/components/ui/button";
+
 export default function Demo() {
+  const { playCorrectTune, playIncorrectTune } = useTune();
   return (
     <div>
       <h1 className="text-2xl font-bold">demo</h1>
+      <Button onClick={playCorrectTune}>Play Correct Tune</Button>
+      <Button onClick={playIncorrectTune}>Play Incorrect Tune</Button>
     </div>
   );
 }
