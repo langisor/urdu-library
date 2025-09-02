30420f7ccd80b073a5722caa3eb5df40fbb3cc7a
farisubuntu <farisubuntu@gmail.com>
2025-09-02 21:30:46 +0300

    update...

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,8 +1,27 @@
 import { NextResponse, NextRequest } from "next/server";
+import { queryClient } from "@/lib/postgres-client";
 
-import { getUser,UserData } from "@/server/actions/getUser";
-// as fallback user
-export async function GET() {
-  return NextResponse.json({ user: await getUser() });
+type UserData = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  role: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  score: number;
+};
+
+async function getUser(id: number) {
+  const user = await queryClient`
+  SELECT * FROM "User" WHERE "id"=${id}
+`;
+  return user[0];
 }
-
+// as fallback user
+export async function GET(request: NextRequest) {
+  const id = request.nextUrl.searchParams.get("uId");
+  const user = await getUser(Number(id));
+  return NextResponse.json(user);
+}
--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -6,7 +6,10 @@ import useSWR from "swr";
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function MondlyPage() {
-  const { data, error, isLoading } = useSWR("/api/user", fetcher);
+  const { data, error, isLoading } = useSWR(`api/user?uId=2`, fetcher);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return <div>{JSON.stringify(data)}</div>;
 }
