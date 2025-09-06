7b95f9c0fd9fd681cb971b48a4bc9fa51ff039b7
farisubuntu <farisubuntu@gmail.com>
2025-09-06 21:50:13 +0300

    update...

--- src/app/mondly/category/[cid]/[lid]/_components/lesson-dashboard.tsx
+++ src/app/mondly/category/[cid]/[lid]/_components/lesson-dashboard.tsx
@@ -5,6 +5,7 @@ import { Progress } from "@/components/ui/progress";
 import { Pen, SquaresIntersect, Star, TextCursor } from "lucide-react";
 import { Quizzer } from "./quizzer";
 import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/general/button";
 type Lesson = {
   id: number;
   index: number;
@@ -30,35 +31,48 @@ export function LessonDashboard({
     <span key={i}>
       <Star className="w-5 h-5 text-yellow-500" />
     </span>
-
   ));
   return (
     <div className="flex flex-col items-center text-right" dir="rtl">
-
-      <div className="w-full mb-6"><Header categoryName={categoryName} categoryID={lessons[0].categoryID} /></div>
-      {/* Lessons Grid */}
-      <BackToUnit /> 
+      <div className="w-full mb-6">
+        {/* <Header categoryName={categoryName} categoryID={lessons[0].categoryID} /> */}
+        <Card>
+          <CardContent className="p-2 flex justify-around items-center">
+            <h2 className="text-2xl font-bold mt-2 text-center mb-5">
+              <Button>{categoryName}</Button>
+            </h2>
+            <BackToUnit />
+          </CardContent>
+        </Card>
+ 
+      </div>
       <div className="flex flex-wrap justify-center gap-2">
         {lessons.map((lesson) => (
           <div key={lesson.id} className="flex flex-col gap-2  w-full sm:w-120">
             <Card dir="rtl" className="mt-4 w-full">
               <CardContent className="p-2 h-full">
-                <h2 className="text-2xl font-bold mt-2 text-center mb-5">{lesson.name}</h2>
+                <h2 className="text-2xl font-bold mt-2 text-center mb-5">
+                  {lesson.name}
+                </h2>
                 <Progress value={lesson.countQuiz} />
                 {/* icons */}
                 <div className="w-full flex gap-2 mx-10">
                   <div className="w-full flex gap-4 mt-8 items-center">
-                    <span className="rounded-lg text-blue-600 bg-gray-100 p-1"><Pen className="h-6 w-6" /></span>
+                    <span className="rounded-lg text-blue-600 bg-gray-100 p-1">
+                      <Pen className="h-6 w-6" />
+                    </span>
                     <span className="text-md">{lesson.countQuiz}</span>
                   </div>
                   <div className="w-full flex gap-4 mt-8 items-center">
-
-                    <span className="rounded-lg text-blue-600 bg-gray-100 p-1"><TextCursor className="h-6 w-6" /></span>
+                    <span className="rounded-lg text-blue-600 bg-gray-100 p-1">
+                      <TextCursor className="h-6 w-6" />
+                    </span>
                     <span className="text-md">{lesson.countWords}</span>
                   </div>
                   <div className="w-full flex gap-4 mt-8 items-center">
-
-                    <span className="rounded-lg text-blue-600 bg-gray-100 p-1"><SquaresIntersect className="h-6 w-6" /></span>
+                    <span className="rounded-lg text-blue-600 bg-gray-100 p-1">
+                      <SquaresIntersect className="h-6 w-6" />
+                    </span>
                     <span className="text-md">{lesson.countPhrases}</span>
                   </div>
                 </div>
