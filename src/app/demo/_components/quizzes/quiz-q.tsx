b667fe3dea7b7cf14b36b2c2b38805e97c9df38c
farisubuntu <farisubuntu@gmail.com>
2025-09-09 14:39:10 +0300

    update...

--- src/app/demo/_components/quizzes/quiz-q.tsx
+++ src/app/demo/_components/quizzes/quiz-q.tsx
@@ -4,6 +4,7 @@ import { QuizQItem } from "../../_hooks/definitions";
 import { convertQ } from "../../_hooks/converters";
 import { useHookstate } from "@hookstate/core";
 import { mainScreenStore } from "../screens/store";
+import { useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { useTune } from "@/hooks/use-tone";
 import { Label } from "@/components/ui/label";
@@ -29,51 +30,57 @@ export default function QuizQ({ quiz }: { quiz: QuizQItem }) {
   const state = useHookstate({
     questions: convertQ(quiz),
     currentQuestionIndex: 0,
+    isLastQuestion: false,
   });
   const { playCorrectTune, playIncorrectTune } = useTune();
   const feedBack = useHookstate<{ isCorrect: boolean; text: string } | null>(
     null
   );
   const mainScreenState = useHookstate(mainScreenStore);
+  const selectectOption = useHookstate<string | null>(null);
   const currentQuestion = state.questions[state.currentQuestionIndex.get()];
-  const isLastQuestion =
-    state.currentQuestionIndex.get() === state.questions.length - 1;
 
   // actions
   const actions = {
-    selectOption: (option: Option) => {
-      console.log(option);
-      // set isAnswered to true
+    nextQuestion: () => {
+      // wait 2 seconds
+      setTimeout(() => {
+        //  if last question
+        if (state.currentQuestionIndex.get() === state.questions.length - 1) {
+          mainScreenState.currentQuizIndex.set((p) => p + 1);
+        } else {
+          state.currentQuestionIndex.set(state.currentQuestionIndex.get() + 1);
+          selectectOption.set(null);
+          currentQuestion.isAnswered.set(false);
+          feedBack.set(null);
+        }
+      }, 2000);
+    },
+    selectOption: (optionId: string) => {
+      console.log(optionId);
+      selectectOption.set(optionId);
       currentQuestion.isAnswered.set(true);
       // check if option is correct
-      if (option.id === currentQuestion.correctAnswerId.get()) {
+      if (optionId === currentQuestion.correctAnswerId.get()) {
         playCorrectTune();
-        feedBack.set({ isCorrect: true, text: "أحسنت" });
         // add score
         mainScreenState.score.set((p) => p + 1);
+        feedBack.set({ isCorrect: true, text: "أحسنت" });
+        actions.nextQuestion();
       } else {
+        playIncorrectTune();
         const correctOption = currentQuestion.options.find(
           (option) => option.id.get() === currentQuestion.correctAnswerId.get()
         );
-        playIncorrectTune();
         feedBack.set({
           isCorrect: false,
           text: "الإجابة الصحيحة هي: " + correctOption?.text.get(),
         });
-      }
-      // if not last question, go to next question after 3 seconds
-      if (!isLastQuestion) {
-        setTimeout(() => {
-          state.currentQuestionIndex.set(state.currentQuestionIndex.get() + 1);
-        }, 3000);
-      } else {
-        //    move to next quiz after 3 seconds
-        setTimeout(() => {
-          mainScreenState.currentQuizIndex.set((p) => p + 1);
-        }, 3000);
+        actions.nextQuestion();
       }
     },
   };
+
   console.log("sample: ", state.questions.get());
   return (
     <div className="flex flex-col  text-right" dir="rtl">
@@ -90,16 +97,20 @@ export default function QuizQ({ quiz }: { quiz: QuizQItem }) {
         </CardHeader>
         {/* options area */}
         <CardContent>
-          <RadioGroup className="flex flex-col gap-2 text-right" dir="rtl">
+          <RadioGroup
+            className="flex flex-col gap-2 text-right"
+            dir="rtl"
+            onValueChange={(value) => actions.selectOption(value)}
+            value={selectectOption.get()}
+          >
             {currentQuestion.options.map((option) => (
-              <Card className="flex flex-row px-2 items-center gap-3 hover:bg-gray-100 cursor-pointer transition-all hover:scale-105 text-xl">
-                <RadioGroupItem
-                  value={option.id.get()}
-                  id={option.id.get()}
-                  onClick={() => actions.selectOption(option.get())}
-                  className=""
-                />
-                <Label htmlFor={option.id.get()}>{option.text.get()}</Label>
+              <Card
+                className={`flex flex-row px-2  gap-3 hover:bg-gray-100 cursor-pointer transition-all hover:scale-105 text-xl  `}
+              >
+                <RadioGroupItem value={option.id.get()} id={option.id.get()} />
+                <Label htmlFor={option.id.get()} className="w-full ">
+                  {option.text.get()}
+                </Label>
               </Card>
             ))}
           </RadioGroup>
