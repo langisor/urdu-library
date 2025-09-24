0ac6df62ae04c1692d4eec34d874bb0361c46301
farisubuntu <farisubuntu@gmail.com>
2025-09-24 15:02:01 +0300

    update...

--- src/app/demo/_stores/global-state.ts
+++ src/app/demo/_stores/global-state.ts
@@ -1,36 +1,39 @@
-import {hookstate,useHookstate,State} from "@hookstate/core";
+import { hookstate, useHookstate, State } from "@hookstate/core";
 
 interface GlobalState {
-    currentQuizIndex: number;
-    totalQuizzes: number;
-    isComplete: boolean;
-    score: number;
+  currentQuizIndex: number;
+  totalQuizzes: number;
+  isComplete: boolean;
+  score: number;
 }
+
 // internal variables
+
 const globalState = hookstate({
-    currentQuizIndex: 0,
-    totalQuizzes: 0,
-    isComplete: false,
-    score: 0,
+  currentQuizIndex: 0,
+  totalQuizzes: 0,
+  isComplete: false,
+  score: 0,
 });
 const wrapState = (s: State<GlobalState>) => ({
-     getCurrentQuizIndex:()=>s.currentQuizIndex.value,
-     getTotalQuizzes:()=>s.totalQuizzes.value,
-     getIsComplete:()=>s.isComplete.value,
-     getScore:()=>s.score.value,
-     setTotalQuizzes:(totalQuizzes:number)=>s.totalQuizzes.set(totalQuizzes),
-     setScore:(score:number)=>s.score.set((p)=>p+score),
-     nextQuiz:()=>s.currentQuizIndex.set((p)=>p+1),
-     prevQuiz:()=>s.currentQuizIndex.set((p)=>p-1),
-     reset:()=>s.set({
-        currentQuizIndex: 0,
-        totalQuizzes: 0,
-        isComplete: false,
-        score: 0,
+  getCurrentQuizIndex: () => s.currentQuizIndex.value,
+  getTotalQuizzes: () => s.totalQuizzes.value,
+  getIsComplete: () => s.isComplete.value,
+  getScore: () => s.score.value,
+  setTotalQuizzes: (totalQuizzes: number) => s.totalQuizzes.set(totalQuizzes),
+  setScore: (score: number) => s.score.set((p) => p + score),
+  nextQuiz: () => s.currentQuizIndex.set((p) => p + 1),
+  prevQuiz: () => s.currentQuizIndex.set((p) => p - 1),
+  reset: () =>
+    s.set({
+      currentQuizIndex: 0,
+      totalQuizzes: 0,
+      isComplete: false,
+      score: 0,
     }),
-})
+});
 
+// exposed functions external to the state
 // The following 2 functions can be exported now:
-export const accessGlobalState = () => wrapState(globalState)
-export const useGlobalState = () => wrapState(useHookstate(globalState))
-
+export const accessGlobalState = () => wrapState(globalState); // Ex. access outside of the component
+export const useGlobalState = () => wrapState(useHookstate(globalState)); // Ex. access inside of the component
