f1130580f7885721f7bcb02b0551e6cf03e5980b
farisubuntu <farisubuntu@gmail.com>
2025-10-03 16:15:59 +0300

    update...

--- src/app/mondly/category/quizzer/_components/quizzes/quiz-c1b.tsx
+++ src/app/mondly/category/quizzer/_components/quizzes/quiz-c1b.tsx
@@ -1,123 +1,123 @@
-"use client";
-import { QuizC1bItem } from "../definitions";
-import { convertC1b } from "../converters";
-import { useHookstate, State } from "@hookstate/core";
+// "use client";
+// import { QuizC1bItem } from "../definitions";
+// import { convertC1b } from "../converters";
+// import { useHookstate, State } from "@hookstate/core";
 
-import { useTune } from "@/hooks/use-tone";
-import { Button } from "@/components/ui/button";
-import { TonePlayerButton } from "@/components/general/tone-button-player";
-import { Card, CardContent, CardDescription } from "@/components/ui/card";
-import { shuffleArray } from "@/lib/helpers";
-import * as React from "react";
-import { Feedback } from "../helpers-types";
+// import { useTune } from "@/hooks/use-tone";
+// import { Button } from "@/components/ui/button";
+// import { TonePlayerButton } from "@/components/general/tone-button-player";
+// import { Card, CardContent, CardDescription } from "@/components/ui/card";
+// import { shuffleArray } from "@/lib/helpers";
+// import * as React from "react";
+// import { Feedback } from "../helpers-types";
 
-interface QuizC1bState {
-  question: QuizC1bItem;
-  quizzerFeedback: State<Feedback>;
-  onNextQuiz: () => void;
-}
-export default function QuizC1b({
-  quiz,
-  quizzerFeedback,
-  onNextQuiz,
-}: {
-  quiz: QuizC1bItem;
-  quizzerFeedback: State<Feedback>;
-  onNextQuiz: () => void;
-}) {
-  const state = useHookstate({
-    question: convertC1b(quiz),
-  });
-  const { playCorrectTune, playIncorrectTune } = useTune();
-  const filledToken: State<{ key: string; text: string }> = useHookstate({
-    key: state.question.sentence.find((t) => t.isHidden)!.key.get(),
-    text: "______",
-  });
-  console.log("quiz", quiz);
-  const checkAnswer = () => {
-    console.log("checkAnswer()...");
-    if (filledToken.key.get() === state.question.sentence.find((t) => t.isHidden)!.key.get()) {
-      playCorrectTune();
-    } else {
-      playIncorrectTune();
-    }     
-  };
+// interface QuizC1bState {
+//   question: QuizC1bItem;
+//   quizzerFeedback: State<Feedback>;
+//   onNextQuiz: () => void;
+// }
+// export default function QuizC1b({
+//   quiz,
+//   quizzerFeedback,
+//   onNextQuiz,
+// }: {
+//   quiz: QuizC1bItem;
+//   quizzerFeedback: State<Feedback>;
+//   onNextQuiz: () => void;
+// }) {
+//   const state = useHookstate({
+//     question: convertC1b(quiz),
+//   });
+//   const { playCorrectTune, playIncorrectTune } = useTune();
+//   const filledToken: State<{ key: string; text: string }> = useHookstate({
+//     key: state.question.sentence.find((t) => t.isHidden)!.key.get(),
+//     text: "______",
+//   });
+//   console.log("quiz", quiz);
+//   const checkAnswer = () => {
+//     console.log("checkAnswer()...");
+//     if (filledToken.key.get() === state.question.sentence.find((t) => t.isHidden)!.key.get()) {
+//       playCorrectTune();
+//     } else {
+//       playIncorrectTune();
+//     }     
+//   };
 
-  const renders = {
-    renderHeader: () => {
-      return (
-        <Card className="px-2 flex flex-row gap-2 items-center py-2">
-          <CardContent className="flex flex-row gap-2 items-center">
-            <div>{state.question.text.get()}</div>
-            <div>
-              <TonePlayerButton url={state.question.audioFile.get()} />
-            </div>
-          </CardContent>
-        </Card>
-      );
-    },
-    renderSentence: () => {
-      const hiddenToken = state.question.sentence.find((t) => t.isHidden);
-      return (
-        <Card className="px-2 flex flex-row gap-2 items-center py-2">
-          <CardContent>
-            {state.question.sentence.map((token, index) =>
-              token.key.get() === filledToken.key.get() ? (
-                <span key={index}>{filledToken.text.get()}</span>
-              ) : (
-                <span key={index}>{token.text.get()}</span>
-              )
-            )}
-          </CardContent>
-        </Card>
-      );
-    },
-    renderTokens: () => {
-      return (
-        <CardContent>
-          {state.question.tokens.get().map((token, index) => (
-            <Button key={index} onClick={() => filledToken.set(token)} className="mr-2 mb-2">
-              {token.text}
-            </Button>
-          ))}
-        </CardContent>
-      );
-    },
-  };
+//   const renders = {
+//     renderHeader: () => {
+//       return (
+//         <Card className="px-2 flex flex-row gap-2 items-center py-2">
+//           <CardContent className="flex flex-row gap-2 items-center">
+//             <div>{state.question.text.get()}</div>
+//             <div>
+//               <TonePlayerButton url={state.question.audioFile.get()} />
+//             </div>
+//           </CardContent>
+//         </Card>
+//       );
+//     },
+//     renderSentence: () => {
+//       const hiddenToken = state.question.sentence.find((t) => t.isHidden);
+//       return (
+//         <Card className="px-2 flex flex-row gap-2 items-center py-2">
+//           <CardContent>
+//             {state.question.sentence.map((token, index) =>
+//               token.key.get() === filledToken.key.get() ? (
+//                 <span key={index}>{filledToken.text.get()}</span>
+//               ) : (
+//                 <span key={index}>{token.text.get()}</span>
+//               )
+//             )}
+//           </CardContent>
+//         </Card>
+//       );
+//     },
+//     renderTokens: () => {
+//       return (
+//         <CardContent>
+//           {state.question.tokens.get().map((token, index) => (
+//             <Button key={index} onClick={() => filledToken.set(token)} className="mr-2 mb-2">
+//               {token.text}
+//             </Button>
+//           ))}
+//         </CardContent>
+//       );
+//     },
+//   };
 
-  console.log("state.question.sentence.get()", state.question.tokens.get());
-  return (
-    <Card className="flex flex-col gap-6 arabic-text px-2">
-      <div>{renders.renderHeader()}</div>
-      <div>{renders.renderSentence()}</div>
-      <div>{renders.renderTokens()}</div>
-      <div className="flex justify-center">
-        <Button
-          disabled={quizzerFeedback.isAnswered.get()}
-          onClick={checkAnswer}
-          className="w-2/3"
-        >
-          تأكد
-        </Button>
-      </div>
-    </Card>
-  );
-}
+//   console.log("state.question.sentence.get()", state.question.tokens.get());
+//   return (
+//     <Card className="flex flex-col gap-6 arabic-text px-2">
+//       <div>{renders.renderHeader()}</div>
+//       <div>{renders.renderSentence()}</div>
+//       <div>{renders.renderTokens()}</div>
+//       <div className="flex justify-center">
+//         <Button
+//           disabled={quizzerFeedback.isAnswered.get()}
+//           onClick={checkAnswer}
+//           className="w-2/3"
+//         >
+//           تأكد
+//         </Button>
+//       </div>
+//     </Card>
+//   );
+// }
 
-interface SentenceProps {
-  sentence: State<{ key: string; text: string; isHidden: boolean }[]>;
-}
-const Sentence = ({ sentence }: SentenceProps) => {
-  const tokens = sentence.get();
-  return (
-    <Card className="px-2 flex flex-row gap-2 items-center py-2">
-      <CardContent>
-        {tokens.map((token, index) => (
-          <span className="mr-2" key={index}>
-            {token.text}
-          </span>
-        ))}
-      </CardContent>
-    </Card>
-  );
-};
+// interface SentenceProps {
+//   sentence: State<{ key: string; text: string; isHidden: boolean }[]>;
+// }
+// const Sentence = ({ sentence }: SentenceProps) => {
+//   const tokens = sentence.get();
+//   return (
+//     <Card className="px-2 flex flex-row gap-2 items-center py-2">
+//       <CardContent>
+//         {tokens.map((token, index) => (
+//           <span className="mr-2" key={index}>
+//             {token.text}
+//           </span>
+//         ))}
+//       </CardContent>
+//     </Card>
+//   );
+// };
--- src/app/mondly/category/quizzer/_components/quizzes/quiz-d.tsx
+++ src/app/mondly/category/quizzer/_components/quizzes/quiz-d.tsx
@@ -1,168 +1,191 @@
-// "use client";
-// import { QuizDItem } from "../definitions";
-// import { convertD } from "../converters";
-// import Image from "next/image";
-// import {
-//   Card,
-//   CardContent,
-//   CardHeader,
-//   CardTitle,
-//   CardFooter,
-// } from "@/components/ui/card";
-// import { useHookstate } from "@hookstate/core";
+"use client";
+import { QuizDItem } from "../definitions";
+import { convertD } from "../converters";
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
+import { useHookstate, State } from "@hookstate/core";
 
-// import { useTune } from "@/hooks/use-tone";
-// import { TonePlayerButton } from "@/components/general/tone-button-player";
+import { useTune } from "@/hooks/use-tone";
+import { TonePlayerButton } from "@/components/general/tone-button-player";
+import {
+  type Question,
+  Feedback,
+  mergeElements,
+  shuffleArray,
+} from "../helpers-types";
+import * as React from "react";
 
+interface QuizProps {
+  question:  QuizDItem;
+  feedback: State<Feedback>;
+  onNext: () => void | null;
+}
 
-// export default function QuizD({ quiz }: { quiz: QuizDItem }) {
-//   const state = useHookstate({
-//     questions: convertD(quiz),
-//     currentQuestionIndex: 0,
+export default function QuizD({
+  question,
+  feedback,
+  onNext,
+}: QuizProps) {
+  const state = useHookstate({
+    questions: convertD(question),
+    currentQuestionIndex: 0,
  
-//   });
+  });
 
-//   const feedBack = useHookstate({
-//     isCorrect: false,
-//     text: "",
-//   });
-//   const { playCorrectTune, playIncorrectTune } = useTune();
-//   const currentQuestion = state.questions[state.currentQuestionIndex.get()];
-//   //  actions
-//   const actions = {
-//     checkAnswer: (answer: string) => {
-//       currentQuestion.isAnswered.set(true);
-//       if (answer === currentQuestion.correctAnswer.get()) {
-//         playCorrectTune();
-//         feedBack.set({ isCorrect: true, text: "أحسنت" });
-//         // add score
-//         // mainScreenState.setScore(mainScreenState.getScore() + 1);
-//       } else {
-//         playIncorrectTune();
-//         feedBack.set({
-//           isCorrect: false,
-//           text: "الإجابة الصحيحة هي: " + currentQuestion.correctAnswer.get(),
-//         });
-//       }
-//       // go to next question if not finished after 3 seconds
-//       if (state.currentQuestionIndex.get() < state.questions.length - 1) {
-//         setTimeout(() => {
-//           state.currentQuestionIndex.set(state.currentQuestionIndex.get() + 1);
-//         }, 1000);
-//       } else {
-//          mainScreenState.nextQuiz();
-//       }
-//     },
-//   };
+  
+  const { playCorrectTune, playIncorrectTune } = useTune();
+  const currentQuestion = state.questions[state.currentQuestionIndex.get()];
+  //  actions
+  const actions = {
+    checkAnswer: (answer: string) => {
+      currentQuestion.isAnswered.set(true);
+      if (answer === currentQuestion.correctAnswer.get()) {
+        playCorrectTune();
+        feedback.set((p)=> {
+          return {
+            ...p,
+            isCorrect: true,
+            message: "أحسنت",
+           
+          };
+        });
+        // add score
+        // mainScreenState.setScore(mainScreenState.getScore() + 1);
+      } else {
+        playIncorrectTune();
+        feedback.set((p)=> {
+          return {
+            ...p,
+            isCorrect: false,
+            message: "الإجابة الصحيحة هي: " + currentQuestion.correctAnswer.get(),
+          };
+        });
+      }
+      // go to next question if not finished after 3 seconds
+      if (state.currentQuestionIndex.get() < state.questions.length - 1) {
+        setTimeout(() => {
+          state.currentQuestionIndex.set(state.currentQuestionIndex.get() + 1);
+        }, 1000);
+      } else {
+        onNext();
+      }
+    },
+  };
  
 
-//   //  renders
-//   const renderCards = () => {
-//     return (
-//       <div className="flex flex-col gap-2 text-right arabic-text">
-//         {/* top options */}
-//         <div className="grid grid-cols-2 gap-2">
-//           <OptionCard
-//             option={currentQuestion.options[0].get()}
-//             onCheckAnswer={actions.checkAnswer}
-//             textTop={true}
-//           />
-//           <OptionCard
-//             option={currentQuestion.options[1].get()}
-//             onCheckAnswer={actions.checkAnswer}
-//             textTop={true}
-//           />
-//         </div>
+  //  renders
+  const renderCards = () => {
+    return (
+      <div className="flex flex-col gap-2 text-right arabic-text">
+        {/* top options */}
+        <div className="grid grid-cols-2 gap-2">
+          <OptionCard
+            option={currentQuestion.options[0].get()}
+            onCheckAnswer={actions.checkAnswer}
+            textTop={true}
+          />
+          <OptionCard
+            option={currentQuestion.options[1].get()}
+            onCheckAnswer={actions.checkAnswer}
+            textTop={true}
+          />
+        </div>
 
-//         {/* question */}
-//         <Card className="flex flex-row gap-2 text-right w-full justify-center items-center ">
-//           <p> {currentQuestion.text.get()}</p>
-//           <TonePlayerButton url={currentQuestion.audioFile.get()} />
-//         </Card>
+        {/* question */}
+        <Card className="flex flex-row gap-2 text-right w-full justify-center items-center ">
+          <p> {currentQuestion.text.get()}</p>
+          <TonePlayerButton url={currentQuestion.audioFile.get()} />
+        </Card>
 
-//         {/* bottom options */}
-//         <div className="grid grid-cols-2 gap-2">
-//           <OptionCard
-//             option={currentQuestion.options[2].get()}
-//             onCheckAnswer={actions.checkAnswer}
-//             textTop={false}
-//           />
-//           <OptionCard
-//             option={currentQuestion.options[3].get()}
-//             onCheckAnswer={actions.checkAnswer}
-//             textTop={false}
-//           />
-//         </div>
-//       </div>
-//     );
-//   };
+        {/* bottom options */}
+        <div className="grid grid-cols-2 gap-2">
+          <OptionCard
+            option={currentQuestion.options[2].get()}
+            onCheckAnswer={actions.checkAnswer}
+            textTop={false}
+          />
+          <OptionCard
+            option={currentQuestion.options[3].get()}
+            onCheckAnswer={actions.checkAnswer}
+            textTop={false}
+          />
+        </div>
+      </div>
+    );
+  };
 
 
-//   if (state.currentQuestionIndex.get() === state.questions.length - 1) {
-//     setTimeout(() => {
-//       mainScreenState.nextQuiz();
-//     }, 1000);
-//   }
-//   return <div>{renderCards()}</div>;
-// }
+  if (state.currentQuestionIndex.get() === state.questions.length - 1) {
+    setTimeout(() => {
+      onNext();
+    }, 1000);
+  }
+  return <div>{renderCards()}</div>;
+}
 
-// interface OptionCardProps {
-//   textTop: boolean;
-//   option: {
-//     id: string;
-//     text: string;
-//     image: string;
-//   };
-//   onCheckAnswer: (answer: string) => void;
-// }
+interface OptionCardProps {
+  textTop: boolean;
+  option: {
+    id: string;
+    text: string;
+    image: string;
+  };
+  onCheckAnswer: (answer: string) => void;
+}
 
-// function OptionCard({ option, onCheckAnswer, textTop }: OptionCardProps) {
-//   console.log("option", option.image);
+function OptionCard({ option, onCheckAnswer, textTop }: OptionCardProps) {
+  console.log("option", option.image);
 
-//   if (textTop) {
-//     return (
-//       <Card
-//         role="button"
-//         onClick={() => onCheckAnswer(option.text)}
-//         className="cursor-pointer hover:scale-105 hover:shadow-2xl"
-//       >
-//         <CardHeader className="flex justify-center items-center">
-//           <CardTitle>{option.text}</CardTitle>
-//         </CardHeader>
-//         <CardContent className="w-full">
-//           <div className="">
-//             <Image
-//               className="object-contain"
-//               src={option.image}
-//               alt={option.text}
-//               width={300}
-//               height={300}
-//             />
-//           </div>
-//         </CardContent>
-//       </Card>
-//     );
-//   }
-//   return (
-//     <Card
-//       role="button"
-//       onClick={() => onCheckAnswer(option.text)}
-//       className="cursor-pointer hover:scale-105 hover:shadow-2xl"
-//     >
-//       <CardContent className="w-full">
-//         <div className="">
-//           <Image
-//             className="object-contain"
-//             src={option.image}
-//             alt={option.text}
-//             width={300}
-//             height={300}
-//           />
-//         </div>
-//       </CardContent>
-//       <CardFooter className="flex justify-center items-center">
-//         <CardTitle>{option.text}</CardTitle>
-//       </CardFooter>
-//     </Card>
-//   );
-// }
+  if (textTop) {
+    return (
+      <Card
+        role="button"
+        onClick={() => onCheckAnswer(option.text)}
+        className="cursor-pointer hover:scale-105 hover:shadow-2xl"
+      >
+        <CardHeader className="flex justify-center items-center">
+          <CardTitle>{option.text}</CardTitle>
+        </CardHeader>
+        <CardContent className="w-full">
+          <div className="">
+            <Image
+              className="object-contain"
+              src={option.image}
+              alt={option.text}
+              width={300}
+              height={300}
+            />
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
+  return (
+    <Card
+      role="button"
+      onClick={() => onCheckAnswer(option.text)}
+      className="cursor-pointer hover:scale-105 hover:shadow-2xl"
+    >
+      <CardContent className="w-full">
+        <div className="">
+          <Image
+            className="object-contain"
+            src={option.image}
+            alt={option.text}
+            width={300}
+            height={300}
+          />
+        </div>
+      </CardContent>
+      <CardFooter className="flex justify-center items-center">
+        <CardTitle>{option.text}</CardTitle>
+      </CardFooter>
+    </Card>
+  );
+}
