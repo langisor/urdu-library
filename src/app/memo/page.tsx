3a7c22ef4cb95fc9f4cc3b7bd61f8927bdbb662e
farisubuntu <farisubuntu@gmail.com>
2025-10-04 23:12:14 +0300

    update...

---
+++ src/app/memo/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import * as React from "react";
+
+export default function MemoPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Memo</h1>
+    </div>
+  );
+}
