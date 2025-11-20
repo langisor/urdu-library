27c3d249b1fd67adc8879b5c0ca024c09ab7cc85
farisubuntu <farisubuntu@gmail.com>
2025-11-20 22:25:56 +0300

    update...

--- src/app/etc/course-book/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import book from "./_components/toc.json";
-import BookBrowser from "./_components/book-browser";
-import { Unit } from "./_components/difinitions";
-
-export default function DemoPage() {
-  return <BookBrowser data={book as Unit[]} />;
-}
